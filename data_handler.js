776e990a751f392c615c4b2603374c2b76191554
Martin Florian <martin.florian@hu-berlin.de>
2021-05-03 20:05:21 +0200

    Display results sequentially + much refactoring

--- data_handler.js
+++ data_handler.js
@@ -1,54 +1,55 @@
-function call_stellarbeat_on_input() {
-	const date = document.getElementById('date');
-	var time = document.getElementById('time');
-    var url = "";
-    if (!time.value) {
-	    url = "https://api.stellarbeat.io/v2/all?at=" + date.value + "T" + "00:00";
-        console.log("url: ", url);
-    } else {
-	    url = "https://api.stellarbeat.io/v2/all?at=" + date.value + "T" + time.value;
-    }
-	timestamp = date.value + "T" + time.value;
-	console.log("input timestamp: ", timestamp );
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.send();
-	xhr.onload = function() {
-		console.log(xhr.status);
-		fbas = xhr.response;
-		prepare_fbas_string(timestamp, fbas);
-		return xhr.response;
-	}
+function get_and_analyze_stellarbeat_data_via_button() {
+	var date = document.getElementById('date').value;
+	var time = document.getElementById('time').value;
+    get_and_analyze_stellarbeat_data(date, time);
 }
 
-function call_stellarbeat_on_click(clicked_date) {
+function get_and_analyze_stellarbeat_data_via_click(clicked_date) {
+    var date = clicked_date;
 	var time = "00" + ":" + "00";
-	var url = "https://api.stellarbeat.io/v2/all?at=" + clicked_date + "T" + time;
-	document.getElementById('date').value = clicked_date;
+	document.getElementById('date').value = date;
 	document.getElementById('time').value = time;
-	console.log(url);
-	timestamp = clicked_date + "T" + time;
-	console.log("input timestamp: ", timestamp );
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.send();
-	xhr.onload = function() {
-		console.log(xhr.status);
-		fbas = xhr.response;
-		prepare_fbas_string(timestamp, fbas);
-		return xhr.response;
-	}
+    get_and_analyze_stellarbeat_data(date, time);
 }
 
-function prepare_fbas_string(timestamp, text) {
-	var js_object = JSON.parse(text);
-	const stellarbeat_nodes = JSON.stringify(js_object[Object.keys(js_object)[0]]);
-	const stellarbeat_orga = JSON.stringify(js_object[Object.keys(js_object)[1]]);
+function get_and_analyze_stellarbeat_data(date, time) {
+    timestamp = build_timestamp(date, time);
+    console.log("input timestamp: ", timestamp);
+    get_stellarbeat_data_promise(date, time)
+    .then(function(stellarbeat_data) {
+        parse_stellarbeat_data(stellarbeat_data);
+        should_merge = merge_selection();
+		analyze_and_update_results_box();
+    });
+}
+
+function build_timestamp(date, time) {
+    return date + "T" + time;
+}
+
+function get_stellarbeat_data_promise(date, time) {
+    var url = "https://api.stellarbeat.io/v2/all?at=" + build_timestamp(date, time);
+    console.log("Getting data from: ", url);
+    return new Promise(function (resolve) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+     	xhr.onload = function() {
+     		console.log(xhr.status);
+     		resolve(xhr.response);
+     	}
+        xhr.send();
+    });
+}
+
+function parse_stellarbeat_data(stellarbeat_data) {
+	var js_object = JSON.parse(stellarbeat_data);
+	stellarbeat_nodes = JSON.stringify(js_object[Object.keys(js_object)[0]]);
+    stellarbeat_orgs = JSON.stringify(js_object[Object.keys(js_object)[1]]);
 	stellarbeat_timestamp = JSON.stringify(js_object[Object.keys(js_object)[3]]).replace(/\"/g, "");
-	console.log(text.length);
+
 	fbas_from_stellarbeat = stellarbeat_nodes;
     current_nodes = fbas_from_stellarbeat;
-    current_orgs = stellarbeat_orga;
+    current_orgs = stellarbeat_orgs;
 
 	var inactive_nodes = [];
 	const nodes_as_obj = JSON.parse(fbas_from_stellarbeat);
@@ -59,40 +60,49 @@ function prepare_fbas_string(timestamp, text) {
 	});
 	const faulty_nodes = JSON.stringify(inactive_nodes);
     inactive_fbas_nodes = faulty_nodes;
+}
 
-	var should_merge = merge_selection();
+async function analyze_and_update_results_box() {
+    var start = performance.now();
 
-	const mqs = () => run_mqs(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-	const mss = () => run_mss(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-	const mbs = () => run_mbs(fbas_from_stellarbeat, stellarbeat_orga, faulty_nodes, should_merge);
-	const tt = () => run_tt(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-    var mqs_res, mss_res, mbs_res, tt_res;
-	var start = performance.now();
-    Promise.all([
-        mqs(),
-        mss(),
-        mbs(),
-        tt(),
-    ]).then(([mqs_res, mss_res, mbs_res, tt_res]) => {
-        var stop = performance.now();
-        const duration = stop - start;
-        const time_as_secs = duration / 1000;
-        var [mqs, mqs_len, mqs_unused, cache_hit] = split_results(mqs_res);
-	    console.log("mqs cache hit: ", cache_hit);
-        var quorum_inter = Object.values(mqs_res)[3];
-        var [mss, mss_len, mss_min, cache_hit] = split_results(mss_res);
-	    console.log("mss cache hit: ", cache_hit);
-        var [mbs, mbs_len, mbs_min, cache_hit] = split_results(mbs_res);
-	    console.log("mbs cache hit: ", cache_hit);
-        var [tt, tt_len, exists, symm_top_tier, cache_hit] = split_top_tier(tt_res);
-	    console.log("tt cache hit: ", cache_hit);
-        var analysis_res = new CompleteResults(mqs, quorum_inter, mqs_len,
-            mbs, mbs_len, mbs_min, mss, mss_len, mss_min,
-            tt, tt_len, exists, symm_top_tier);
-        log_results(timestamp, stellarbeat_timestamp, analysis_res, time_as_secs)
-    }).catch((err) => console.log(err))
+    clear_results_box();
+    show_results_box_title(timestamp, stellarbeat_timestamp);
+    await tick();
 
-	window.prepare_fbas_string = prepare_fbas_string
+    console.log("mqs analysis...");
+    let mqs_res = await run_mqs(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [mqs, mqs_len, mqs_unused, cache_hit] = split_results(mqs_res);
+    console.log("mqs cache hit: ", cache_hit);
+    var has_quorum_intersection = Object.values(mqs_res)[3];
+    show_mqs_results(mqs, mqs_len, has_quorum_intersection);
+    await tick();
+
+    console.log("mbs analysis...");
+    let mbs_res = await run_mbs(fbas_from_stellarbeat, stellarbeat_orgs, inactive_fbas_nodes, should_merge);
+    var [mbs, mbs_len, mbs_min, cache_hit] = split_results(mbs_res);
+    console.log("mbs cache hit: ", cache_hit);
+    show_mbs_results(mbs, mbs_len, mbs_min);
+    await tick();
+
+    console.log("mss analysis...");
+	let mss_res = await run_mss(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [mss, mss_len, mss_min, cache_hit] = split_results(mss_res);
+    console.log("mss cache hit: ", cache_hit);
+    show_mss_results(mss, mss_len, mss_min);
+    await tick();
+
+    console.log("tt analysis...");
+	let tt_res = await run_tt(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [tt, tt_len, symm_tt_exists, symm_tt, cache_hit] = split_top_tier(tt_res);
+    console.log("tt cache hit: ", cache_hit);
+    show_tt_results(tt, tt_len, symm_tt_exists, symm_tt);
+    await tick();
+
+    var stop = performance.now();
+    const duration = stop - start;
+    const time_as_secs = duration / 1000;
+	console.log("analysis duration (s): ", time_as_secs);
+    await tick();
 }
 
 function split_results(result) {
