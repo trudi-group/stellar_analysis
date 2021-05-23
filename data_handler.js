trudi-group/stellar_analysis

commit 776e990a751f392c615c4b2603374c2b76191554

Display results sequentially + much refactoring

data_handler.js
@@ -1,54 +1,55 @@
-function call_stellarbeat_on_input() {
-	const date = document.getElementById('date');
-	var time = document.getElementById('time');
-    var url = "";
-    if (!time.value) {
-	    url = "https://api.stellarbeat.io/v2/all?at=" + date.value + "T" + "00:00";
-        console.log("url: ", url);
-    } else {
-	    url = "https://api.stellarbeat.io/v2/all?at=" + date.value + "T" + time.value;
-    }
-	timestamp = date.value + "T" + time.value;
-	console.log("input timestamp: ", timestamp );
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.send();
-	xhr.onload = function() {
-		console.log(xhr.status);
-		fbas = xhr.response;
-		prepare_fbas_string(timestamp, fbas);
-		return xhr.response;
-	}
+function get_and_analyze_stellarbeat_data_via_button() {
+	var date = document.getElementById('date').value;
+	var time = document.getElementById('time').value;
+    get_and_analyze_stellarbeat_data(date, time);
 }
 
-function call_stellarbeat_on_click(clicked_date) {
+function get_and_analyze_stellarbeat_data_via_click(clicked_date) {
+    var date = clicked_date;
 	var time = "00" + ":" + "00";
-	var url = "https://api.stellarbeat.io/v2/all?at=" + clicked_date + "T" + time;
-	document.getElementById('date').value = clicked_date;
+	document.getElementById('date').value = date;
 	document.getElementById('time').value = time;
-	console.log(url);
-	timestamp = clicked_date + "T" + time;
-	console.log("input timestamp: ", timestamp );
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.send();
-	xhr.onload = function() {
-		console.log(xhr.status);
-		fbas = xhr.response;
-		prepare_fbas_string(timestamp, fbas);
-		return xhr.response;
-	}
+    get_and_analyze_stellarbeat_data(date, time);
 }
 
-function prepare_fbas_string(timestamp, text) {
-	var js_object = JSON.parse(text);
-	const stellarbeat_nodes = JSON.stringify(js_object[Object.keys(js_object)[0]]);
-	const stellarbeat_orga = JSON.stringify(js_object[Object.keys(js_object)[1]]);
+function get_and_analyze_stellarbeat_data(date, time) {
+    timestamp = build_timestamp(date, time);
+    console.log("input timestamp: ", timestamp);
+    get_stellarbeat_data_promise(date, time)
+    .then(function(stellarbeat_data) {
+        parse_stellarbeat_data(stellarbeat_data);
+        should_merge = merge_selection();
+		analyze_and_update_results_box();
+    });
+}
+
+function build_timestamp(date, time) {
+    return date + "T" + time;
+}
+
+function get_stellarbeat_data_promise(date, time) {
+    var url = "https://api.stellarbeat.io/v2/all?at=" + build_timestamp(date, time);
+    console.log("Getting data from: ", url);
+    return new Promise(function (resolve) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+     	xhr.onload = function() {
+     		console.log(xhr.status);
+     		resolve(xhr.response);
+     	}
+        xhr.send();
+    });
+}
+
+function parse_stellarbeat_data(stellarbeat_data) {
+	var js_object = JSON.parse(stellarbeat_data);
+	stellarbeat_nodes = JSON.stringify(js_object[Object.keys(js_object)[0]]);
+    stellarbeat_orgs = JSON.stringify(js_object[Object.keys(js_object)[1]]);
 	stellarbeat_timestamp = JSON.stringify(js_object[Object.keys(js_object)[3]]).replace(/\"/g, "");
-	console.log(text.length);
+
 	fbas_from_stellarbeat = stellarbeat_nodes;
     current_nodes = fbas_from_stellarbeat;
-    current_orgs = stellarbeat_orga;
+    current_orgs = stellarbeat_orgs;
 
 	var inactive_nodes = [];
 	const nodes_as_obj = JSON.parse(fbas_from_stellarbeat);
@@ -59,40 +60,49 @@ function prepare_fbas_string(timestamp, text) {
 	});
 	const faulty_nodes = JSON.stringify(inactive_nodes);
     inactive_fbas_nodes = faulty_nodes;
+}
 
-	var should_merge = merge_selection();
+async function analyze_and_update_results_box() {
+    var start = performance.now();
 
-	const mqs = () => run_mqs(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-	const mss = () => run_mss(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-	const mbs = () => run_mbs(fbas_from_stellarbeat, stellarbeat_orga, faulty_nodes, should_merge);
-	const tt = () => run_tt(fbas_from_stellarbeat, stellarbeat_orga, should_merge);
-    var mqs_res, mss_res, mbs_res, tt_res;
-	var start = performance.now();
-    Promise.all([
-        mqs(),
-        mss(),
-        mbs(),
-        tt(),
-    ]).then(([mqs_res, mss_res, mbs_res, tt_res]) => {
-        var stop = performance.now();
-        const duration = stop - start;
-        const time_as_secs = duration / 1000;
-        var [mqs, mqs_len, mqs_unused, cache_hit] = split_results(mqs_res);
-	    console.log("mqs cache hit: ", cache_hit);
-        var quorum_inter = Object.values(mqs_res)[3];
-        var [mss, mss_len, mss_min, cache_hit] = split_results(mss_res);
-	    console.log("mss cache hit: ", cache_hit);
-        var [mbs, mbs_len, mbs_min, cache_hit] = split_results(mbs_res);
-	    console.log("mbs cache hit: ", cache_hit);
-        var [tt, tt_len, exists, symm_top_tier, cache_hit] = split_top_tier(tt_res);
-	    console.log("tt cache hit: ", cache_hit);
-        var analysis_res = new CompleteResults(mqs, quorum_inter, mqs_len,
-            mbs, mbs_len, mbs_min, mss, mss_len, mss_min,
-            tt, tt_len, exists, symm_top_tier);
-        log_results(timestamp, stellarbeat_timestamp, analysis_res, time_as_secs)
-    }).catch((err) => console.log(err))
+    clear_results_box();
+    show_results_box_title(timestamp, stellarbeat_timestamp);
+    await tick();
 
-	window.prepare_fbas_string = prepare_fbas_string
+    console.log("mqs analysis...");
+    let mqs_res = await run_mqs(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [mqs, mqs_len, mqs_unused, cache_hit] = split_results(mqs_res);
+    console.log("mqs cache hit: ", cache_hit);
+    var has_quorum_intersection = Object.values(mqs_res)[3];
+    show_mqs_results(mqs, mqs_len, has_quorum_intersection);
+    await tick();
+
+    console.log("mbs analysis...");
+    let mbs_res = await run_mbs(fbas_from_stellarbeat, stellarbeat_orgs, inactive_fbas_nodes, should_merge);
+    var [mbs, mbs_len, mbs_min, cache_hit] = split_results(mbs_res);
+    console.log("mbs cache hit: ", cache_hit);
+    show_mbs_results(mbs, mbs_len, mbs_min);
+    await tick();
+
+    console.log("mss analysis...");
+	let mss_res = await run_mss(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [mss, mss_len, mss_min, cache_hit] = split_results(mss_res);
+    console.log("mss cache hit: ", cache_hit);
+    show_mss_results(mss, mss_len, mss_min);
+    await tick();
+
+    console.log("tt analysis...");
+	let tt_res = await run_tt(fbas_from_stellarbeat, stellarbeat_orgs, should_merge);
+    var [tt, tt_len, symm_tt_exists, symm_tt, cache_hit] = split_top_tier(tt_res);
+    console.log("tt cache hit: ", cache_hit);
+    show_tt_results(tt, tt_len, symm_tt_exists, symm_tt);
+    await tick();
+
+    var stop = performance.now();
+    const duration = stop - start;
+    const time_as_secs = duration / 1000;
+	console.log("analysis duration (s): ", time_as_secs);
+    await tick();
 }
 
 function split_results(result) {

graph.js
@@ -302,7 +302,7 @@ function make_chart_from_csv_data(canvas_id, csv_data) {
 			var label = chart.data.labels[clickedElementindex];
 			var value = chart.data.datasets[clickedDatasetIndex].data[clickedElementindex];     
 			console.log("Clicked: " + label + " - " + value);
-			call_stellarbeat_on_click(label);
+			get_and_analyze_stellarbeat_data_via_click(label);
 		}
 	};
 }

index.html
@@ -47,7 +47,7 @@
     <div class="control-box">
       <input type="date" id="date" name="date" min="2019-07-01">
       <input type="time" id="time" name="time" value="00:00">
-      <button id="submit-btn" type="button" onclick="call_stellarbeat_on_input()">
+      <button id="submit-btn" type="button" onclick="get_and_analyze_stellarbeat_data_via_button()">
         Analyze!
       </button>
       <script>

util.js
@@ -7,42 +7,17 @@ function maybe_change_analysis() {
 	if (last_merge_state != should_merge) {
         last_merge_state = should_merge;
 
-        const mqs = () => run_mqs(current_nodes, current_orgs, should_merge);
-        const mss = () => run_mss(current_nodes, current_orgs, should_merge);
-        const mbs = () => run_mbs(current_nodes, current_orgs, inactive_fbas_nodes, should_merge);
-        const tt = () => run_tt(current_nodes, current_orgs, should_merge);
-        var mqs_res, mss_res, mbs_res, tt_res;
-		var start = performance.now();
-        Promise.all([
-            mqs(),
-            mss(),
-            mbs(),
-            tt(),
-        ]).then(([mqs_res, mss_res, mbs_res, tt_res]) => {
-            var stop = performance.now();
-            const duration = stop - start;
-            const time_as_secs = duration / 1000;
-            var [mqs, mqs_len, mqs_unused, cache_hit] = split_results(mqs_res);
-            console.log("mqs cache hit: ", cache_hit);
-            var quorum_inter = Object.values(mqs_res)[3];
-            var [mss, mss_len, mss_min, cache_hit] = split_results(mss_res);
-            console.log("mss cache hit: ", cache_hit);
-            var [mbs, mbs_len, mbs_min, cache_hit] = split_results(mbs_res);
-            console.log("mbs cache hit: ", cache_hit);
-            var [tt, tt_len, exists, symm_top_tier, cache_hit] = split_top_tier(tt_res);
-            console.log("tt cache hit: ", cache_hit);
-            var analysis_res = new CompleteResults(mqs, quorum_inter, mqs_len,
-                mbs, mbs_len, mbs_min, mss, mss_len, mss_min,
-                tt, tt_len, exists, symm_top_tier);
-            log_results(timestamp, stellarbeat_timestamp, analysis_res, time_as_secs)
-        }).catch((err) => console.log(err))
+        analyze_and_update_results_box();
 	}
 }
 
 const div = document.getElementById('results-box');
-function write_to_div(text) {
-	div.innerHTML = div.innerHTML + text;
-	div.innerHTML = div.innerHTML + "<br>";
+
+function create_title_in_div(text) {
+    var b = document.createElement("b");
+    b.innerHTML = text;
+	div.append(b);
+    div.append(document.createElement("br"));
 }
 
 function create_buttons_in_div(title, short_results, results, tooltip) {
@@ -59,6 +34,16 @@ function create_buttons_in_div(title, short_results, results, tooltip) {
 	var label = document.createTextNode(short_results);
 	btn.appendChild(label);
 
+	btn.addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight){
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	});
+
 	var output = document.getElementById("results-box");
 	output.appendChild(btn);
 
@@ -72,6 +57,19 @@ function create_buttons_in_div(title, short_results, results, tooltip) {
 	copy_btn.setAttribute('class', 'copyButton');
 	copy_btn.setAttribute('title', 'Copy');
 	copy_btn.innerHTML = "&#x2398";
+
+	copy_btn.addEventListener("click", function() {
+		var textarea = document.createElement('textarea');
+		textarea.id = 'temp_element';
+		textarea.style.height = 0;
+		document.body.appendChild(textarea);
+		textarea.value = this.nextElementSibling.innerText;
+		var selector = document.querySelector('#temp_element');
+		selector.select();
+		document.execCommand('copy');
+		document.body.removeChild(textarea);
+	});
+
 	paragraph.appendChild(copy_btn);
 
 	var myDiv = document.createElement("div");
@@ -103,6 +101,16 @@ function create_buttons_in_div_alter(title, short_results, sub_results, results,
 	text = document.createTextNode(sub_results);
 	btn.appendChild(text);
 
+	btn.addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight){
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	});
+
 	var output = document.getElementById("results-box");
 	output.appendChild(btn);
 
@@ -112,10 +120,24 @@ function create_buttons_in_div_alter(title, short_results, sub_results, results,
 
 	var paragraph = document.createElement("P");
 	paragraph.setAttribute('id', 'par');
+
 	var copy_btn = document.createElement("button");
 	copy_btn.setAttribute('class', 'copyButton');
 	copy_btn.setAttribute('title', 'Copy');
 	copy_btn.innerHTML = "&#x2398";
+
+	copy_btn.addEventListener("click", function() {
+		var textarea = document.createElement('textarea');
+		textarea.id = 'temp_element';
+		textarea.style.height = 0;
+		document.body.appendChild(textarea);
+		textarea.value = this.nextElementSibling.innerText;
+		var selector = document.querySelector('#temp_element');
+		selector.select();
+		document.execCommand('copy');
+		document.body.removeChild(textarea);
+	});
+
 	paragraph.appendChild(copy_btn);
 
 	var myDiv = document.createElement("pre");
@@ -125,165 +147,53 @@ function create_buttons_in_div_alter(title, short_results, sub_results, results,
 	inner_div.appendChild(paragraph);
 }
 
-function log_results(timestamp, stellarbeat_timestamp, results, duration) {
+function clear_results_box() {
+    while(div.firstChild) {
+        div.removeChild(div.firstChild);
+    };
+}
 
-	div.innerHTML = "";
+function show_results_box_title(timestamp, stellarbeat_timestamp) {
+	create_title_in_div("Results for " + timestamp + " (data from Stellarbeat has timestamp " + stellarbeat_timestamp + ")");
+}
 
-	write_to_div("<b>Results for " + timestamp + " (data from Stellarbeat has timestamp " + stellarbeat_timestamp + ")" + "</b>");
-	var mqs_output = [JSON.stringify(results.minimal_quorums, null, 4).replace(/\\/g, "").slice(1, -1)];
+function show_mqs_results(mqs, mqs_len, has_quorum_intersection) {
+	var mqs_output = [JSON.stringify(mqs, null, 4).replace(/\\/g, "").slice(1, -1)];
 
 	var mqs_tooltip = "Minimal sets of nodes that are sufficient to reach agreement.";
-	if (results.has_intersection) {
-		create_buttons_in_div("quorums |", " We found " + results.minimal_quorums_size +" minimal quorums. All quorums intersect 👍", mqs_output, mqs_tooltip); 
+	if (has_quorum_intersection) {
+		create_buttons_in_div("quorums |", " We found " + mqs_len + " minimal quorums. All quorums intersect 👍", mqs_output, mqs_tooltip);
 	} else {
-		create_buttons_in_div("quorums |", " We found " + results.minimal_quorums_size +" minimal quorums. Some quorums don't intersect 👎 Safety severely threatened for some nodes! (Also, the remaining results here might not make much sense.)", mqs_output, mqs_tooltip);
+		create_buttons_in_div("quorums |", " We found " + mqs_len + " minimal quorums. Some quorums don't intersect 👎 Safety severely threatened for some nodes! (Also, the remaining results here might not make much sense.)", mqs_output, mqs_tooltip);
 	}
+}
 
-	var mbs_output = [JSON.stringify(results.minimal_blocking_sets, null, 4).replace(/\\/g, "").slice(1, -1)];
+function show_mbs_results(mbs, mbs_len, mbs_min) {
+	var mbs_output = [JSON.stringify(mbs, null, 4).replace(/\\/g, "").slice(1, -1)];
 	var mbs_tooltip = "Control over any of these sets is sufficient to compromise the liveness of all nodes and to censor future transactions.";
-	create_buttons_in_div("blocking sets |", " We found "+ results.minimal_blocking_sets_size + " minimal blocking sets. The smallest one has size " + results.smallest_blocking_set_size + ".", mbs_output, mbs_tooltip);
+	create_buttons_in_div("blocking sets |", " We found "+ mbs_len + " minimal blocking sets. The smallest one has size " + mbs_min + ".", mbs_output, mbs_tooltip);
+}
 
-	var mss_output = [JSON.stringify(results.minimal_splitting_sets, null, 4).replace(/\\/g, "").slice(1, -1)];
+function show_mss_results(mss, mss_len, mss_min) {
+	var mss_output = [JSON.stringify(mss, null, 4).replace(/\\/g, "").slice(1, -1)];
 	var mss_tooltip = "Control over any of these sets is sufficient to compromise safety by undermining the quorum intersection of at least two quorums.";
-	create_buttons_in_div("splitting sets |", " We found " + results.minimal_splitting_sets_size + " minimal splitting sets. The smallest one has size " + results.smallest_splitting_set_size + ".", mss_output, mss_tooltip);
+	create_buttons_in_div("splitting sets |", " We found "+ mss_len + " minimal blocking sets. The smallest one has size " + mss_min + ".", mss_output, mss_tooltip);
+}
 
-	var top_tier = [JSON.stringify(results.top_tier)];
+function show_tt_results(tt, tt_len, symm_tt_exists, symm_tt) {
+	var tt = [JSON.stringify(tt)];
 	var tt_tooltip = "These are the nodes out of which all minimal quorums, minimal blocking sets and minimal splitting sets are formed.";
-	create_buttons_in_div("top tier 👑 |", " There are " + results.top_tier_size +  " nodes in the top tier.", top_tier, tt_tooltip);
+	create_buttons_in_div("top tier 👑 |", " There are " + tt_len +  " nodes in the top tier.", tt, tt_tooltip);
 
-	if (results.symmetric_top_tier_exists) {
-		var symmetric_top_tier = results.symmetric_top_tier.slice(1, -1);
+	if (symm_tt_exists) {
+		var symmetric_top_tier = symm_tt.slice(1, -1);
 		symmetric_top_tier = JSON.stringify(JSON.parse(symmetric_top_tier), null, 4);
 		var sc_tooltip = "All top tier nodes have identical quorum sets."
 		create_buttons_in_div_alter("The top tier is", " symmetric", ".", symmetric_top_tier, sc_tooltip);
 	}
-
-	console.log("analysis duration (s): ", duration);
-
-	var coll = document.getElementsByClassName("collapsible");
-	coll[0].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-	coll[1].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-	coll[2].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-	coll[3].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-
-	if (coll.length > 4) {
-		coll[4].addEventListener("click", function() {
-			this.classList.toggle("active");
-			var content = this.nextElementSibling;
-			if (content.style.maxHeight){
-				content.style.maxHeight = null;
-			} else {
-				content.style.maxHeight = content.scrollHeight + "px";
-			}
-		});
-	}
-
-	var copy_btns = document.getElementsByClassName("copyButton");
-	copy_btns[0].addEventListener("click", function() {
-		var textarea = document.createElement('textarea');
-		textarea.id = 'temp_element';
-		textarea.style.height = 0;
-		document.body.appendChild(textarea);
-		textarea.value = this.nextElementSibling.innerText;
-		var selector = document.querySelector('#temp_element');
-		selector.select();
-		document.execCommand('copy');
-		document.body.removeChild(textarea);
-	});
-	copy_btns[1].addEventListener("click", function() {
-		var textarea = document.createElement('textarea');
-		textarea.id = 'temp_element';
-		textarea.style.height = 0;
-		document.body.appendChild(textarea);
-		textarea.value = this.nextElementSibling.innerText;
-		var selector = document.querySelector('#temp_element');
-		selector.select();
-		document.execCommand('copy');
-		document.body.removeChild(textarea);
-	});
-	copy_btns[2].addEventListener("click", function() {
-		var textarea = document.createElement('textarea');
-		textarea.id = 'temp_element';
-		textarea.style.height = 0;
-		document.body.appendChild(textarea);
-		textarea.value = this.nextElementSibling.innerText;
-		var selector = document.querySelector('#temp_element');
-		selector.select();
-		document.execCommand('copy');
-		document.body.removeChild(textarea);
-	});
-	copy_btns[3].addEventListener("click", function() {
-		var textarea = document.createElement('textarea');
-		textarea.id = 'temp_element';
-		textarea.style.height = 0;
-		document.body.appendChild(textarea);
-		textarea.value = this.nextElementSibling.innerText;
-		var selector = document.querySelector('#temp_element');
-		selector.select();
-		document.execCommand('copy');
-		document.body.removeChild(textarea);
-	});
-
-	if (copy_btns.length > 4) {
-		copy_btns[4].addEventListener("click", function() {
-			var textarea = document.createElement('textarea');
-			textarea.id = 'temp_element';
-			textarea.style.height = 0;
-			document.body.appendChild(textarea);
-			textarea.value = this.nextElementSibling.innerText;
-			var selector = document.querySelector('#temp_element');
-			selector.select();
-			document.execCommand('copy');
-			document.body.removeChild(textarea);
-		});
-		}
 }
 
-function CompleteResults(mqs, mqs_inter, mqs_len, mbs, mbs_len, mbs_min, mss, mss_len, mss_min, top_tier, top_tier_size, symmetric_top_tier_exists, symmetric_top_tier) {
-
-    this.minimal_quorums = mqs;
-    this.has_intersection = mqs_inter;
-    this.minimal_quorums_size = mqs_len;
-    this.minimal_blocking_sets = mbs;
-    this.minimal_blocking_sets_size = mbs_len;
-    this.smallest_blocking_set_size = mbs_min;
-    this.minimal_splitting_sets = mss;
-    this.minimal_splitting_sets_size = mss_len;
-    this.smallest_splitting_set_size = mss_min;
-    this.top_tier = top_tier;
-    this.top_tier_size = top_tier_size;
-    this.symmetric_top_tier_exists = symmetric_top_tier_exists;
-    this.symmetric_top_tier = symmetric_top_tier;
+// let a few milliseconds pass just so, so that the browser can redraw
+function tick() {
+    return new Promise(r => setTimeout(r, 1));
 }

commit c3b1a479ac7f2e5d19afdd87d54fa61ccdbbd252

Fix bug introduced in 776e990a when time field was left empty

data_handler.js
@@ -1,6 +1,9 @@
 function get_and_analyze_stellarbeat_data_via_button() {
 	var date = document.getElementById('date').value;
 	var time = document.getElementById('time').value;
+    if (!time.length) {
+        time = "00" + ":" + "00";
+    }
     get_and_analyze_stellarbeat_data(date, time);
 }
 